f8f5ff87ff8b84789da6aec4687a90882b4d944b
Agung Nugraha <agung.nugraha@live.com>
2021-04-26 21:49:38 +0700

    temp change

---
+++ app/Http/Livewire/Biodata.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component; 
+
+class Biodata extends Component
+{
+    public function render()
+    {
+        return view('livewire.biodata');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Livewire\Psytest;
 use App\Http\Livewire\Schedule;
 use App\Http\Livewire\Teacherlist;
 use App\Http\Livewire\Question;
+use App\Http\Livewire\Biodata;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +21,8 @@ use App\Http\Livewire\Question;
 */
 
 Route::get('/question', Question::class);
+
+Route::get('/biodata', Biodata::class);
 
 
 Route::get('/', function () {
